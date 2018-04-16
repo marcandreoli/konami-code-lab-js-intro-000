4692a14e6c5e58dfb9d9b0c7f28129eae6688881
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-16 22:59:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-input.addEventListenet('keydown', function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
+  document.addEventListenet('keydown', function (e) {
+
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -13,4 +15,4 @@ input.addEventListenet('keydown', function init(e) {
       index = 0;
     }
   }
-}
+})
